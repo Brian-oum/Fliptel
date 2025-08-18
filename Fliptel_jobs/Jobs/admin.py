6379161299958906a3959a760aa7227133524f18
Brian-oum <brianobanda08@gmail.com>
2025-08-18 11:34:17 +0300

    import in the admin page

--- Fliptel_jobs/Jobs/admin.py
+++ Fliptel_jobs/Jobs/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 from .models import Job
 
 @admin.register(Job)
-class JobAdmin(admin.ModelAdmin):
+class JobAdmin(ImportExportModelAdmin):
     list_display = ('title', 'company', 'location', 'job_type', 'posted_date')
     search_fields = ('title', 'company', 'location')
     list_filter = ('job_type', 'posted_date')
-
